a916a67541cf07bb5c5b199e63b12e4618ee5187
LincWong <236241416@qq.com>
2019-05-15 10:26:09 +0800

    fix: webpack build bug

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,13 @@ module.exports = {
     entry: './lib/index.js',
     output: {
         filename: 'index.js',
+        libraryTarget: 'umd',
+        globalObject: "this",
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new cleanWebpackPlugin(),
         new webpack.NormalModuleReplacementPlugin(/^engine.io-client$/, 'engine.io-client-weex'),
-    ]
+    ],
+    mode: 'production',
 };
